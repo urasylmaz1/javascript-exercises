0dbc97de1afcdae28925e31d900f0ccbea017a93
urasylmaz1 <urasylmaz1@gmail.com>
2025-08-17 13:16:18 +0300

    finished exercises 1 to 7

--- 07_reverseString/reverseString.js
+++ 07_reverseString/reverseString.js
@@ -1,4 +1,12 @@
-const reverseString = function() {
+const reverseString = function(str) {
+    let result = '';
+    const length = str.length;
+
+    for (let i = length - 1; i >= 0; i--) {
+        result += str[i];
+    }
+    
+    return result;
 
 };
 
